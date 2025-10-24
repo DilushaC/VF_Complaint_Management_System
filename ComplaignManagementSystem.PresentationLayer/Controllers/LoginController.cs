4a1f8da3cac3030096cb389138a0c592c02843af
4537 <4537@VF-CO-IT-DEV-11>
2025-10-24 11:38:33 +0530

    Login HMC implemented intiial stage completed

---
+++ ComplaignManagementSystem.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using ComplaintManagementSystem.Business.LoginHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComplaignManagementSystem.Presentation.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Explicitly load the view from Views/User/Login.cshtml
+            return View("~/Views/User/Login.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            var user = await _loginService.ValidateUserAsync(username, password);
+
+            if (user != null)
+            {
+                // Example: Set session or redirect
+                HttpContext.Session.SetString("UserName", user.UserName);
+                return RedirectToAction("Dashboard", "ComplaintManageProcess");
+            }
+
+            // If login failed, show same User/Login view again with error
+            ViewBag.Error = "Invalid username or password.";
+            return View("~/Views/User/Login.cshtml");
+        }
+    }
+}
---
+++ ComplaintManagementSystem.Business/Helpers/PasswordHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplaintManagementSystem.Business.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string ComputeHmac(string salt, string password)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(salt);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            using (var hmac = new HMACSHA256(keyBytes))
+            {
+                var hashBytes = hmac.ComputeHash(passwordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public static bool VerifyPassword(string enteredPassword, string storedHash, string storedSalt)
+        {
+            var computedHash = ComputeHmac(storedSalt, enteredPassword);
+            return storedHash == computedHash;
+        }
+    }
+}
