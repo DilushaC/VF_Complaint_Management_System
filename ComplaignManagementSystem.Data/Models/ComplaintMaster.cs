fa48641c85eac12105d0d0da4be33ea636dea3dd
4536 <4536@VF-CO-IT-DEV-02>
2025-10-30 11:17:25 +0530

    Upgrade Solution

--- ComplaignManagementSystem.Data/Models/ComplaintMaster.cs
+++ ComplaignManagementSystem.Data/Models/ComplaintMaster.cs
@@ -13,6 +13,8 @@ namespace ComplaignManagementSystem.Data.Models
         public string Method { get; set; }
         public string Complaint { get; set; }
         public string CreatedUser { get; set; }
+        public string Department { get; set; }
+        public string Nature { get; set; }
         public string Branch { get; set; }
         public string Priority { get; set; }
         public string Status { get; set; }
