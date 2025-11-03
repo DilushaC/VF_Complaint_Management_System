1738547188b4e849b43854fe549e91b71e35eb4a
4537 <4537@VF-CO-IT-DEV-11>
2025-11-03 10:47:37 +0530

    removed unwanted comments

--- ComplaignManagementSystem.PresentationLayer/Program.cs
+++ ComplaignManagementSystem.PresentationLayer/Program.cs
@@ -7,20 +7,17 @@ using ComplaignManagementSystem.Presentation.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add controllers and global session filter
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add<SessionCheckAttribute>();
 });
 
-// Register dependencies
 builder.Services.AddScoped<DapperContext>();
 builder.Services.AddScoped<_ConnectionService>();
 builder.Services.AddScoped<IComplaintManageProcessService, ComplaintManageProcessService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddHttpContextAccessor();
 
-// Configure session
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(1);
@@ -30,7 +27,6 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
-// Environment setup
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -46,7 +42,6 @@ app.UseSession();
 
 app.UseAuthorization();
 
-// Default route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=User}/{action=Login}/{id?}");
